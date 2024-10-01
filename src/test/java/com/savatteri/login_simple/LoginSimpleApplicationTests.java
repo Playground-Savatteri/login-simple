b3a7e23e4f00e9c2ecb24e281c0687940294df0b
giuseppe.savatteri <giuseppe.savatteri@selecta.it>
2024-10-01 09:11:54 +0200

    First commit

---
+++ src/test/java/com/savatteri/login_simple/LoginSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savatteri.login_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
